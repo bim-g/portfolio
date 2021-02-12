3cb4c20e536b2bc36d8304c0fdbe1f905463af34
bim-g <ibmussafb@gmail.com>
2021-02-13 01:12:29 +0200

    re-organised social link

--- js/index.js
+++ js/index.js
@@ -8,4 +8,12 @@ let social=[];
         let education = res.education;       
         w3.displayObject("curriculum", res);
     });
+    w3.getHttpObject("./js/data.json",(res)=>{ 
+        let contact=res.contact;       
+        let identiry = res.identity;       
+        let social = res.social;       
+        let experience = res.experience;       
+        let education = res.education;       
+        w3.displayObject("socialmedia", res);
+    });
 })();
